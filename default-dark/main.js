358ee1f97fee64edf8d4add33043109d6f450dfa
homunculus <107266027+homunculus09@users.noreply.github.com>
2024-09-28 14:13:54 +0200

    Update main.js

--- default-dark/main.js
+++ default-dark/main.js
@@ -1,1 +1,20 @@
+javascript: (function() {
+    // Check if the current URL is 'extranet.ecolemoser.ch'
+    if (window.location.hostname === 'extranet.ecolemoser.ch') {
+        var cssUrl = 'https://raw.githubusercontent.com/homunculus09/extranet-themes/refs/heads/main/default-dark/default-dark-stylesheet.css';
 
+        fetch(cssUrl)
+            .then(response => response.text())
+            .then(cssContent => {
+                var oldStyles = document.querySelectorAll('link[rel="stylesheet"], style');
+                oldStyles.forEach(style => style.remove());
+
+                var styleElement = document.createElement('style');
+                styleElement.innerHTML = cssContent;
+                document.head.appendChild(styleElement);
+            })
+            .catch(error => console.error('Failed to load CSS:', error));
+    } else {
+        console.warn('This script can only be run on extranet.ecolemoser.ch');
+    }
+})();
